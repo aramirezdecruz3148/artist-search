f1d867c9b984761a238f1edb942fae9d426a8ae1
aramirezdecruz3148 <leigh3148@gmail.com>
2019-08-21 13:35:42 -0700

    built out my FindArtistContainer

---
+++ src/components/containers/artist-search-container/FindArtistContainer.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import { getArtists } from '../../../services/getArtistsName';
+import SearchArtist from '../../search-artist/SearchArtist';
+import Artists from '../../artists/Artists';
+
+export default class FindArtistContainer extends class {
+  state = {
+    artistArray: [],
+    artist: '',
+    loading: true, 
+    error: null
+  }
+
+  onInputChange = ({ target }) => {
+    this.setState({ artist: target.value})
+  }
+
+  fetchArtists = () => {
+    return getArtists(this.state.artist) 
+      .then(({ artists }) => {
+        this.setState({ 
+          artistArray: artists, 
+          loading: false 
+        });
+      })
+      .catch(err => this.setState({ 
+        error: err, 
+        loading: true
+      }));
+  }
+
+  onButtonClick = () => {
+    return  this.fetchArtists();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if(prevState.artist !== this.state.artist) return this.fetchArtists();
+  }
+
+  render() {
+    const { 
+      artistArray, 
+      artist, 
+      loading, 
+      error 
+    } = this.state;
+
+    if(error) return <h1>Unable to load artists...</h1>
+    if(loading) return <img alt='gif of someone listening to music' src='https://media.tenor.com/images/23110dfb65a7f1e3a52a02c41dcc7d2d/tenor.gif'/>
+    
+    return (
+      <>
+        <SearchArtist 
+          artist={artist} 
+          onButtonClick ={this.onButtonClick} 
+          onInputChange={this.onInputChange} 
+        />
+        <Artists artistArray={artistArray} />
+      </>
+    );
+  }
+}
--- src/components/search-artist/SearchArtist.js
+++ src/components/search-artist/SearchArtist.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function SearchArtist({ artist, onInputChange }) {
+function SearchArtist({ artist, onInputChange, onButtonClick }) {
   return (
     <>
-      <input type='text' name='artist' value={artist} />
-      <button onClick={onInputChange}>Search</button>
+      <input type='text' name='artist' value={artist} onChange={onInputChange} />
+      <button onClick={onButtonClick}>Search</button>
     </>
   );
 }
 
 SearchArtist.propTypes = {
   artist: PropTypes.string.isRequired,
-  onInputChange: PropTypes.func.isRequired
+  onInputChange: PropTypes.func.isRequired,
+  onButtonClick: PropTypes.func.isRequired
 };
 
 export default SearchArtist;
