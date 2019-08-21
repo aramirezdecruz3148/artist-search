85d02874d4e12f61d72c8426efc66585928a1aa9
aramirezdecruz3148 <leigh3148@gmail.com>
2019-08-21 15:35:37 -0700

    I plugged my paging component into my FindArtistContainer, ammended my original service so it takes a page and puts it in the offset, added page to state in my container and created the two functions to make the buttons work

--- src/components/containers/artist-search-container/FindArtistContainer.js
+++ src/components/containers/artist-search-container/FindArtistContainer.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import { getArtists } from '../../../services/getArtistsName';
 import SearchArtist from '../../search-artist/SearchArtist';
 import Artists from '../../artists/Artists';
+import Paging from '../../paging/Paging';
 
 export default class FindArtistContainer extends Component {
   state = {
     artistArray: [],
     artist: '',
     loading: false, 
-    error: null
+    error: null, 
+    page: 1
   }
 
   onInputChange = ({ target }) => {
@@ -16,7 +18,7 @@ export default class FindArtistContainer extends Component {
   }
 
   fetchArtists = () => {
-    return getArtists(this.state.artist) 
+    return getArtists(this.state.artist, this.state.page) 
       .then(({ singers }) => {
         this.setState({ 
           artistArray: singers, 
@@ -34,16 +36,36 @@ export default class FindArtistContainer extends Component {
     return  this.fetchArtists();
   }
 
+  increasePageCount = () => {
+    this.setState(state => {
+      return ({
+        page: state.page + 1
+      });
+    });
+  }
+
+  decreasePageCount = () => {
+    this.setState(state => {
+      return ({
+        page: state.page - 1
+      });
+    });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if(prevState.page !== this.state.page) return this.fetchArtists();
+  }
+
   render() {
     const { 
       artistArray, 
       artist, 
       loading, 
-      error 
+      error
     } = this.state;
 
-    if(error) return <h1>Unable to load artists...</h1>
-    if(loading) return <img alt='gif of someone listening to music' src='https://media.tenor.com/images/23110dfb65a7f1e3a52a02c41dcc7d2d/tenor.gif'/>
+    if(error) return <h1>Unable to load artists...</h1>;
+    if(loading) return <img alt='gif of someone listening to music' src='https://media.tenor.com/images/23110dfb65a7f1e3a52a02c41dcc7d2d/tenor.gif'/>;
     
     return (
       <>
@@ -52,6 +74,7 @@ export default class FindArtistContainer extends Component {
           onButtonClick ={this.onButtonClick} 
           onInputChange={this.onInputChange} 
         />
+        <Paging onClickPrevious={this.decreasePageCount} onClickNext={this.increasePageCount} />
         <Artists artistArray={artistArray} />
       </>
     );
--- src/services/getArtistsName.js
+++ src/services/getArtistsName.js
@@ -1,5 +1,5 @@
-export const getArtists = (artistName) => {
-  return fetch(`http://musicbrainz.org/ws/2/artist?query=${artistName}&fmt=json&limit=25`)
+export const getArtists = (artistName, page) => {
+  return fetch(`http://musicbrainz.org/ws/2/artist?query=${artistName}&fmt=json&limit=25&offset=${(page - 1) * 25}`)
     .then(res => {
       if(!res.ok) throw 'Unable to load artists, try again!';
 
