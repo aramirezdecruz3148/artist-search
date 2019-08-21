738e3aedb1a5f12f0e1736e04da5bffe213a9535
aramirezdecruz3148 <leigh3148@gmail.com>
2019-08-21 16:16:35 -0700

    I began adding ReactRouter logic in my app to be able to make my artist names linkable

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import FindArtistContainer from './containers/artist-search-container/FindArtistContainer';
 
 export default function App() {
   return (
     <>
-      <FindArtistContainer />
+    <Router>
+      <Route path='/' component={FindArtistContainer} />
+    </Router>
     </>
   );
 }
