ffd7b9b177b624719c3c242b1303f4f893be5e7d
aramirezdecruz3148 <leigh3148@gmail.com>
2019-08-21 12:04:03 -0700

    I wrote the first pass at my first api service, still need to check it, but commiting before lunch

---
+++ src/services/getArtistsName.js
@@ -0,0 +1,18 @@
+export const getArtists = (artistName) => {
+  return fetch(`http://musicbrainz.org/ws/2/artist?query=${artistName}&fmt=json&limit=25`)
+    .then(res => {
+      if(!res.ok) throw 'Unable to load artists, try again!';
+
+      return res.json();
+    })
+    .then(({ artists }) => {
+      const singers = artists.map(singer => ({
+        artistId: singer.id,
+        artistName: singer.name,
+        artistDeets: singer.disambiguation
+      }));
+      return {
+        singers
+      };
+    });
+};
