4703f0e9e855ad406f6d0f03bed242599e6109b6
aramirezdecruz3148 <leigh3148@gmail.com>
2019-08-21 20:21:44 -0700

    I created the file structure for the next view and list that shows the releases, I have put the bare bones in each

---
+++ src/components/releases/Release.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Release() {
+  return;
+}
+
+Release.propTypes = {
+
+};
+
+export default Release;
---
+++ src/components/releases/Releases.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Releases() {
+  return;
+}
+
+Releases.propTypes = {
+
+};
+
+export default Releases;
