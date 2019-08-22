536b67fec58c143867a1e48c2c208c7db981a969
aramirezdecruz3148 <leigh3148@gmail.com>
2019-08-22 14:44:31 -0700

    I updated my snapshot for Release

--- src/components/releases/Release.test.js
+++ src/components/releases/Release.test.js
@@ -4,7 +4,7 @@ import Release from './Release';
 
 describe('Release component', () => {
   it('renders Release', () => {
-    const wrapper = shallow(<Release releaseId='234j2h3b4j2' releaseDate='March 14th, 1988' releaseTitle='My life' />);
+    const wrapper = shallow(<Release releaseCover='string' releaseId='234j2h3b4j2' releaseDate='March 14th, 1988' releaseTitle='My life' />);
     expect(wrapper).toMatchSnapshot();
   });
 });
