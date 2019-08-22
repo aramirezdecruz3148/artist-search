04963667778b2895ac33b05040064be18a0d92df
aramirezdecruz3148 <leigh3148@gmail.com>
2019-08-22 07:24:52 -0700

    got Release fleshed out and created a snapshot test for it

--- src/components/releases/Release.js
+++ src/components/releases/Release.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Release() {
-  return;
+function Release({ releaseId, releaseTitle, releaseDate }) {
+  return (
+    <>
+      <img src={`http://coverartarchive.org/release/${releaseId}/front`} />
+      <h3>{releaseTitle}</h3>
+      <h5>Date released: {releaseDate}</h5>
+    </>
+  );
 }
 
 Release.propTypes = {
-  //going to need releaseId, releaseTitle, releaseDate
+  releaseId: PropTypes.string.isRequired,
+  releaseTitle: PropTypes.string.isRequired,
+  releaseDate: PropTypes.string.isRequired
 };
 
 export default Release;
---
+++ src/components/releases/Release.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Release from './Release';
+
+describe('Release component', () => {
+  it('renders Release', () => {
+    const wrapper = shallow(<Release releaseId='234j2h3b4j2' releaseDate='March 14th, 1988' releaseTitle='My life' />);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
