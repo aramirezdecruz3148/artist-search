038b0d8f5d48209c3611f95600190ca9ebf5cf8f
aramirezdecruz3148 <leigh3148@gmail.com>
2019-08-22 13:18:18 -0700

    I updated the snapshot in Paging

--- src/components/paging/Paging.test.js
+++ src/components/paging/Paging.test.js
@@ -4,7 +4,7 @@ import Paging from './Paging';
 
 describe('Paging component', () => {
   it('renders Paging', () => {
-    const wrapper = shallow(<Paging onClickPrevious={() => {}} onClickNext={() => {}} />);
+    const wrapper = shallow(<Paging onClickPrevious={() => {}} onClickNext={() => {}} disableNext={9} disablePrev={9} currentPage={10} totalPages={3}/>);
     expect(wrapper).toMatchSnapshot();
   });
 });
