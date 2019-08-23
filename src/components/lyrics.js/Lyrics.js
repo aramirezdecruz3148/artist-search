520f6e1c05ab741676135cf104969e70e9d8b724
aramirezdecruz3148 <leigh3148@gmail.com>
2019-08-23 11:29:18 -0700

    finished styling theLyric component

--- src/components/lyrics.js/Lyrics.js
+++ src/components/lyrics.js/Lyrics.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './Lyrics.css';
 
 function Lyrics({ title, lyrics, artistName }) {
   return (
     <section>
-      <h3>Lyrics for {title} by {artistName}</h3>
-      <pre>{lyrics}</pre>
+      <h3 className={styles.lyricsh3}>Lyrics &nbsp;&nbsp;for &nbsp;&nbsp;<em>{title}</em>&nbsp;&nbsp;&nbsp; by &nbsp;{artistName}</h3>
+      <pre className={styles.lyricspre}>{lyrics}</pre>
     </section>
   );
 }
