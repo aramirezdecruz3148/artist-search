b0576499cd080668789fd178bf38c9dc3b091718
aramirezdecruz3148 <leigh3148@gmail.com>
2019-08-23 09:56:36 -0700

    just styled my nav button

--- src/components/containers/nav/Nav.js
+++ src/components/containers/nav/Nav.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import styles from './Nav.css';
 
 export default function Nav() {
   return (
-    <div>
+    <div className={styles.nav}>
       <a href='/'><button>Search Artists</button></a>
     </div>
   );
