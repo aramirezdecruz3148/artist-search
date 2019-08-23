417e4a9198ab250a43faf57b77a0e6ede88c2a6f
aramirezdecruz3148 <leigh3148@gmail.com>
2019-08-23 14:29:18 -0700

    updated all snapshots

--- src/components/lyrics.js/Lyrics.test.js
+++ src/components/lyrics.js/Lyrics.test.js
@@ -4,7 +4,7 @@ import Lyrics from './Lyrics';
 
 describe('Lyrics component', () => {
   it('renders Lyrics', () => {
-    const wrapper = shallow(<Lyrics title='title' lyrics='lyrics' />);
+    const wrapper = shallow(<Lyrics artistName='werw' title='title' lyrics='lyrics' />);
     expect(wrapper).toMatchSnapshot();
   });
 });
